faf654ecff6d0eefceea77d28e22bd2045e77fea
Burritoad <theburrito@gmail.com>
2023-01-23 18:35:21 -0500

    Add up and down buttons
    Buttons to cycle through the potential keys

--- code.py
+++ code.py
@@ -9,8 +9,19 @@ currentKey = 0
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 led.value = False
+#Button Setup
 button = digitalio.DigitalInOut(board.BUTTON)
 button.switch_to_input(pull=digitalio.Pull.UP)
+upLow = digitalio.DigitalInOut(board.D12)
+upLow.direction = digitalio.Direction.OUTPUT
+upLow = False
+downLow = digitalio.DigitalInOut(board.A3)
+downLow.direction = digitalio.Direction.OUTPUT
+downLow = False
+upButton = digitalio.DigitalInOut(board.D10)
+upButton.switch_to_input(pull=digitalio.Pull.UP)
+downButton = digitalio.DigitalInOut(board.A5)
+downButton.switch_to_input(pull=digitalio.Pull.UP)
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
@@ -25,6 +36,22 @@ while True:
         led.value = True
         layout.write(keyList[currentKey].rstrip())
         led.value = False
-        time.sleep(0.01)  # debounce delay
+        time.sleep(0.05)  # debounce delay
         while not button.value:
+            pass
+    if not downButton.value:
+        currentKey += 1
+        if currentKey > len(keyList) - 1:
+            currentKey = 0
+        print(keyList[currentKey])
+        time.sleep(0.05)  # debounce delay
+        while not downButton.value:
+            pass
+    if not upButton.value:
+        currentKey -= 1
+        if currentKey < 0:
+            currentKey = len(keyList) - 1
+        print(keyList[currentKey])
+        time.sleep(0.05)  # debounce delay
+        while not upButton.value:
             pass
