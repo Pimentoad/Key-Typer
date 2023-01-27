0d9b2634868cff194443b439b27ed3d2ea292230
Burritoad <theburrito@gmail.com>
2023-01-27 10:09:53 -0500

    Button improvements
    Move buttons and increase debounce delays

--- boot.py
+++ boot.py
@@ -3,17 +3,14 @@ import digitalio
 import storage
 import usb_cdc
 import usb_hid
-upLow = digitalio.DigitalInOut(board.D12)
-upLow.direction = digitalio.Direction.OUTPUT
-upLow = False
-downLow = digitalio.DigitalInOut(board.A3)
+downLow = digitalio.DigitalInOut(board.A2)
 downLow.direction = digitalio.Direction.OUTPUT
-downLow = False
+downLow.value = False
 upButton = digitalio.DigitalInOut(board.D10)
-upButton.switch_to_input(pull=digitalio.Pull.UP)
-downButton = digitalio.DigitalInOut(board.A5)
+upButton.switch_to_input(pull=digitalio.Pull.DOWN)
+downButton = digitalio.DigitalInOut(board.A4)
 downButton.switch_to_input(pull=digitalio.Pull.UP)
 usb_hid.enable((usb_hid.Device.KEYBOARD,))
-if upButton.value and downButton:
+if not upButton.value and downButton.value:
     storage.disable_usb_drive()
     usb_cdc.disable()
--- code.py
+++ code.py
@@ -12,16 +12,13 @@ led.value = False
 #Button Setup
 button = digitalio.DigitalInOut(board.BUTTON)
 button.switch_to_input(pull=digitalio.Pull.UP)
-upLow = digitalio.DigitalInOut(board.D12)
-upLow.direction = digitalio.Direction.OUTPUT
-upLow = False
-downLow = digitalio.DigitalInOut(board.A3)
+downLow = digitalio.DigitalInOut(board.A2)
 downLow.direction = digitalio.Direction.OUTPUT
-downLow = False
-upButton = digitalio.DigitalInOut(board.D10)
-upButton.switch_to_input(pull=digitalio.Pull.UP)
-downButton = digitalio.DigitalInOut(board.A5)
-downButton.switch_to_input(pull=digitalio.Pull.UP)
+downLow.value = True
+upButton = digitalio.DigitalInOut(board.D12)
+upButton.switch_to_input(pull=digitalio.Pull.DOWN)
+downButton = digitalio.DigitalInOut(board.A4)
+downButton.switch_to_input(pull=digitalio.Pull.DOWN)
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
@@ -32,6 +29,7 @@ keyFile.close()
 print("read", len(keyList), "keys")
 
 while True:
+    time.sleep(0.1)  # debounce delay
     if not button.value:
         led.value = True
         layout.write(keyList[currentKey].rstrip())
@@ -39,19 +37,19 @@ while True:
         time.sleep(0.05)  # debounce delay
         while not button.value:
             pass
-    if not downButton.value and upButton.value:
+    if downButton.value and not upButton.value:
         currentKey += 1
         if currentKey > len(keyList) - 1:
             currentKey = 0
         print(keyList[currentKey])
-        time.sleep(0.05)  # debounce delay
-        while not downButton.value:
+        time.sleep(0.1)  # debounce delay
+        while downButton.value:
             pass
-    if not upButton.value and downButton.value:
+    if upButton.value and not downButton.value:
         currentKey -= 1
         if currentKey < 0:
             currentKey = len(keyList) - 1
         print(keyList[currentKey])
-        time.sleep(0.05)  # debounce delay
-        while not upButton.value:
+        text_area.text = keyList[currentKey]
+        while upButton.value:
             pass
