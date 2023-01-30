9b24a9946cd72b99b9bfb4a5cedb00143d4b1184
Burritoad <theburrito@gmail.com>
2023-01-30 16:09:46 -0500

    Fix boot.py
    Button state / reading was wrong

--- boot.py
+++ boot.py
@@ -5,7 +5,7 @@ import usb_cdc
 import usb_hid
 downLow = digitalio.DigitalInOut(board.A3)
 downLow.direction = digitalio.Direction.OUTPUT
-downLow.value = False
+downLow.value = True
 upLow = digitalio.DigitalInOut(board.D13)
 upLow.direction = digitalio.Direction.OUTPUT
 upLow.value = True
@@ -14,6 +14,6 @@ upButton.switch_to_input(pull=digitalio.Pull.DOWN)
 downButton = digitalio.DigitalInOut(board.A5)
 downButton.switch_to_input(pull=digitalio.Pull.UP)
 usb_hid.enable((usb_hid.Device.KEYBOARD,usb_hid.Device.MOUSE,))
-if upButton.value and downButton.value:
+if not upButton.value or not downButton.value:
     storage.disable_usb_drive()
     usb_cdc.disable()
